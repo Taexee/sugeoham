3f1f8c7b6aa360f698275aba48dbd0482ebbca8d
Taexee <hee66265@naver.com>
2024-02-06 16:54:40 +0900

    initial commit

---
+++ src/main/java/com/example/sugeoham/SugeohamApplication.java
@@ -0,0 +1,13 @@
+package com.example.sugeoham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugeohamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SugeohamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sugeoham/SugeohamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sugeoham;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SugeohamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
