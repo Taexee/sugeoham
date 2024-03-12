b0423ece70fc3b1d97c79423428121ae7e84947a
Taexee <hee66265@naver.com>
2024-03-12 15:38:00 +0900

    상품 조회 기능 구현

--- product-order-service/src/main/java/com/example/demo/product/ProductPort.java
+++ product-order-service/src/main/java/com/example/demo/product/ProductPort.java
@@ -2,4 +2,6 @@ package com.example.demo.product;
 
 interface ProductPort {
     void save(Product product);
+
+    Product getProduct(Long productId);
 }
--- product-order-service/src/test/java/com/example/demo/product/ProductServiceTest.java
+++ product-order-service/src/test/java/com/example/demo/product/ProductServiceTest.java
@@ -1,9 +1,11 @@
 package com.example.demo.product;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest
 public class ProductServiceTest {
@@ -12,17 +14,16 @@ public class ProductServiceTest {
     private ProductService productService;
 
     @Test
-    void 상품등록(){
-        final AddProductRequest request = 상품등록요청_생성();
+    void 상품조회(){
+        // 상품 등록
+        productService.addProduct(ProductSteps.상품등록요청_생성());
+        final long productId = 1L;
 
-        productService.addProduct(request);
-    }
+        // 상품 조회
+        final GetProductResponse response = productService.getProduct(productId);
 
-    private AddProductRequest 상품등록요청_생성() {
-        final String name = "상품명";
-        final int price = 1000;
-        final DiscountPolicy discountPolicy = DiscountPolicy.NONE;
-        return new AddProductRequest(name, price, discountPolicy);
+        // 응답 검증
+        assertThat(response).isNotNull();
     }
 
 }
