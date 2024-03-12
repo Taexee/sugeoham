af6b68ce61c0336a954c6e61b2085dfd0cdfca67
Taexee <hee66265@naver.com>
2024-03-12 14:34:49 +0900

    jpa 적용하기

--- product-order-service/src/main/java/com/example/demo/product/ProductRepository.java
+++ product-order-service/src/main/java/com/example/demo/product/ProductRepository.java
@@ -1,18 +1,10 @@
 package com.example.demo.product;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Repository
-class ProductRepository {
-    private Long sequece = 0L;
-    private Map<Long, Product> persistence = new HashMap<>();
-
-    public void save(Product product) {
-
-        product.assignId(++sequece);
-        persistence.put(product.getId(), product);
-    }
+interface ProductRepository extends JpaRepository<Product, Long> {
 }
