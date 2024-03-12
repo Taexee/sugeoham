eea6ed8b8bd993ab2517e41047e5b537b7f87406
Taexee <hee66265@naver.com>
2024-03-12 10:21:17 +0900

    스프링부트 테스트로 전환 스프링 빈으로 등록해서 테스트

--- product-order-service/src/main/java/com/example/demo/product/ProductRepository.java
+++ product-order-service/src/main/java/com/example/demo/product/ProductRepository.java
@@ -1,8 +1,11 @@
 package com.example.demo.product;
 
+import org.springframework.stereotype.Repository;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Repository
 class ProductRepository {
     private Long sequece = 0L;
     private Map<Long, Product> persistence = new HashMap<>();
