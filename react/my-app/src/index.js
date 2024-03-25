ef6d5c05e605eba0c6be9e178cc9701f199ba521
Taexee <hee66265@naver.com>
2024-03-26 03:38:55 +0900

    처음 만난 react #6

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import NotificationList from './chapter_06/NotificationList';
 
-import Library from './chapter_03/Library';
-import Clock from './chapter_04/Clock';
-import CommentList from './chapter_05/CommentList';
-
+// chapter_04
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // setInterval(() => {
 //   root.render(
@@ -17,10 +14,19 @@ import CommentList from './chapter_05/CommentList';
 //   );
 // }, 1000);
 
+// chapter_05
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <CommentList />
+//   </React.StrictMode>
+// );
+
+//chapter_06
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CommentList />
+    <NotificationList />
   </React.StrictMode>
 );
 
